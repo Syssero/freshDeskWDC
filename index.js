cb8b36c1f7d18f9c416339e2940186e45bc431b8
Syssero <135151101+Syssero@users.noreply.github.com>
2023-10-24 15:22:42 -0500

    Update index.js
    Testing 2 months

--- index.js
+++ index.js
@@ -227,7 +227,7 @@
             //console.log(agent_array[0])
             $.ajax({
                 type: "GET",
-                url: `https://syssero.freshdesk.com/api/v2/tickets?updated_since=2023-04-23&page=${x}&per_page=100&include=stats,requester,company`,
+                url: `https://syssero.freshdesk.com/api/v2/tickets?updated_since=2023-08-23&page=${x}&per_page=100&include=stats,requester,company`,
                 dataType: 'json',
                 headers: {
                     "Authorization": "Basic " + btoa(apiKey + ":123")
