0bd3676ea3b56b3898bd40db208df0db116925a8
Syssero <135151101+Syssero@users.noreply.github.com>
2023-10-25 16:55:30 -0500

    Update index.js
    Modified the validation

--- index.js
+++ index.js
@@ -306,7 +306,7 @@
                     }
 
                     table.appendRows(tableData);
-					if(x === sleepCount * 20) {
+					if(x == sleepCount * 20) {
 						sleepCount++;
 						sleep(3000);
 					}
