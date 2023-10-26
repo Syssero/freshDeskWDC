7b66d0e75f35f14febc2683525288987febb0dbe
Syssero <135151101+Syssero@users.noreply.github.com>
2023-10-26 12:44:11 -0500

    Update index.js
    20 pages update

--- index.js
+++ index.js
@@ -306,7 +306,7 @@
                     }
 
                     table.appendRows(tableData);
-					/*if(x === sleepCount * 10) {
+					/*if(x === sleepCount * 20) {
 						sleepCount++;
 						sleep(60000);
 					}*/
